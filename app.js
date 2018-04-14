dd187d4bdd1fe12f5a99a586047e7408761f32fa
rcvink <rrvink@gmail.com>
2018-04-14 14:48:05 +0100

    Add typeorm

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('reflect-metadata');
 
 // require routers
 var indexRouter = require('./routes/index');
