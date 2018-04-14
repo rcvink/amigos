d532ae84cef7183531a2762bbe8eea95a465133f
rcvink <rrvink@gmail.com>
2018-04-14 18:08:41 +0100

    Add database connection

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('reflect-metadata');
+var typeorm = require('typeorm');
+
+// create connection
+typeorm.createConnection();
 
 // require routers
 var indexRouter = require('./routes/index');
