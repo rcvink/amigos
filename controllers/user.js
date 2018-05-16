fd7f3410261cb5a9d2b3e4daf6174eda03a6fd48
rcvink <rrvink@gmail.com>
2018-05-16 20:07:14 +0100

    Backup changes

---
+++ controllers/user.js
@@ -0,0 +1,14 @@
+// Display list of all Users.
+exports.userList = function(req, res) {
+    res.send('NOT IMPLEMENTED: user list');
+};
+
+// Display detail page for a specific User.
+exports.userDetail = function(req, res) {
+    res.send('NOT IMPLEMENTED: User detail: ' + req.params.id);
+};
+
+// Handle Author create on POST.
+exports.userCreate = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author create POST');
+};
